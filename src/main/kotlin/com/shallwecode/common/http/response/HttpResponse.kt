4793ea195e28973b9a2a8fcecd67a79be279a58d
choi woo seok <cwooseok60@gmail.com>
2022-04-30 15:36:32 +0900

    Revert "다른 브랜치 작업 개발 브랜치에 merge"
    This reverts commit b3d3008ff1c6e59f85246ca919496cc89b25fd8c.

--- src/main/kotlin/com/shallwecode/common/http/response/HttpResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.shallwecode.common.http.response
-
-class HttpResponse<T>(
-    val message: String? = null,
-    val body: T? = null
-)
--- src/main/kotlin/com/shallwecode/config/mongo/template/MongoTemplateWrapper.kt
+++
@@ -1,7 +0,0 @@
-package com.shallwecode.config.mongo.template
-
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-
-interface MongoTemplateWrapper {
-    val template: ReactiveMongoTemplate
-}
