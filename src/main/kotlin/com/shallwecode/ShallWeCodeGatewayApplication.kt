94dd2a2ebc23cab8e8acdf52a8edbccb8813170c
choi woo seok <cwooseok60@gmail.com>
2022-05-02 00:58:27 +0900

    @EnableConfigurationProperties 인수에 JwtProperties 추가

--- src/main/kotlin/com/shallwecode/ShallWeCodeGatewayApplication.kt
+++ src/main/kotlin/com/shallwecode/ShallWeCodeGatewayApplication.kt
@@ -1,11 +1,12 @@
 package com.shallwecode
 
 import com.shallwecode.certification.authentication.config.redis.RedisConfigProperties
+import com.shallwecode.certification.jwt.config.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-@EnableConfigurationProperties(RedisConfigProperties::class)
+@EnableConfigurationProperties(value = [RedisConfigProperties::class, JwtProperties::class])
 @SpringBootApplication
 class ShallWeCodeGatewayApplication
 
